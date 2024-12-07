b09f3e0683d1bfab3e04554853d88ca9118ed2ba
Skandan C Y <165555723+skandanyal@users.noreply.github.com>
2024-12-07 18:49:57 +0530

    Create trial.py
    an excuse to learn binder

---
+++ trial.py
@@ -0,0 +1,14 @@
+# my first time learning how to use git, github, binder
+# I wrote this file just out of fun
+
+def addition(a, b):
+  return a+b
+
+def subtract(a,b):
+  return a-b
+
+def main():
+  printf(f" If you add 1 to 2, we get {addition(1,2)}.")
+  printf(f"If we subtrat 1 from 2, we get {subtract(2,1).}")
+
+main()
